3bcf37b33594e0fcb804523340c0f72785a1a273
Joseph W Becher <jwbecher@gmail.com>
2013-12-28 10:59:43 -0500

    whois tab now uses the new doTab function

---
+++ assets/js/core.js
@@ -0,0 +1,35 @@
+var dns360_form_urls = {};
+
+dns360_form_urls = {
+    "formWhois": "dowhodig.php?"
+};
+
+
+var dns360_form_fields = {};
+
+dns360_form_fields.formWhois = {
+    "host": "domainNameWhois"
+};
+
+function doTab(tabId, resultId) {
+
+    //alert(Object.keys(dns360_form_fields[tabId]).length);
+
+    var queryString = '';
+
+    jQuery.each(Object.keys(dns360_form_fields[tabId]), function(i, val) {
+        queryString += "&" + val + "=" + $(eval(dns360_form_fields[tabId][val])).val();
+    });
+
+    var ajaxUrl = dns360_form_urls[tabId] + queryString.substring(1);
+
+    //alert(ajaxUrl);
+
+    $.get(ajaxUrl, function(data) {
+        $(eval(resultId)).val(data);
+    });
+
+    return false;
+}
+
+
