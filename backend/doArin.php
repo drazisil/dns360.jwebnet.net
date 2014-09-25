25c7fdb3b36dd115ec5e640fb7b641029282c46c
Joseph Becher <jwbecher@gmail.com>
2014-09-25 13:04:59 -0400

    added in the n part of the command since we are getting away from raw data.

--- backend/doArin.php
+++ backend/doArin.php
@@ -5,6 +5,7 @@ $domain = $_REQUEST["host"];
 $fgtld = $_REQUEST["fgtld"];
 $whois_server = "whois.arin.net";
 
+
 function ae_whois($query, $server)
 {
     define('AE_WHOIS_TIMEOUT', 15); // connection timeout
@@ -15,6 +16,7 @@ function ae_whois($query, $server)
     if (!$f) return false; // connection failed
 
     // sending query
+    $query = 'n ' . $query;
     fwrite($f, $query . "\r\n");
 
     // receving response
